94c4dd7dc4afb9ba5d9923190ad4b6efe7e781b0
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-26 22:57:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,10 @@
-require 'pry'
 def line(katz_deli_line)
   if katz_deli_line.length == 0
     puts "The line is currently empty."
   else
     line = "The line is currently:"
     katz_deli_line.each.with_index(1) do |person, i|
-      binding.pry
-      line << "#{i}. #{person}."
-    
+    line << "#{i}. #{person}."
     end
     puts line
     end
